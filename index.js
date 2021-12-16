0bb76e4cdd54a753c2201a462e830092998b0420
Jordan <mask@DESKTOP-RLTF3LJ.localdomain>
2021-12-15 21:29:25 -0700

    added routes middlewhere

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const router = require('./routes')
 
-app.use(express.json())
+app.use(express.json());
+app.use('/api', router);
 
 app.listen(
     PORT,
---
+++ routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+
+router.post('/exhibiciones', function(req, res){
+    const {id, nombre, descripcion, desde, hasta} = req.body;
+    console.log(req.body);
+    const sql = 'INSERT INTO `exhibiciones` set ?';
+
+    conn.query(sql, function (err, result){
+        if(err){
+            res.status(500).json({
+                success: false,
+                message: 'Hubo un error al insertar el exhibición.'
+            })
+        }
+
+        res.json({sucess: true, message: "OK"});
+
+        });
+    }
+);
