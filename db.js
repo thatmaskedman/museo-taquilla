fd136e3c8c9cb17237e0158b98ad9394ee02bd33
Jordan <mask@DESKTOP-RLTF3LJ.localdomain>
2021-12-15 21:17:57 -0700

    added db connection

---
+++ db.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+}); 
+
+module.exports = con
