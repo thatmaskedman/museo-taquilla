7ae8611c622d90a30e7ac32ee85c8e0bc9646fd5
ger88555 <germanadolf@protonmail.com>
2021-12-21 02:11:03 -0700

    Refactor: exhibitions POST endpoint

--- routes.js
+++ routes.js
@@ -1,27 +1,24 @@
-const { conn } = require('./db')
+const { query } = require('./db/utils');
 const router = require('express').Router()
 
 router.post('/exhibiciones', function(req, res){
-    // try{
-        const sql = 'INSERT INTO `exhibiciones` set ?';
-        console.log(req.body); 
-        conn.query(sql, req.body,function (err, result){
-            if (err) {
-                res.status(500).json({
-                    success: false,
-                    message: 'Hubo un error al insertar el exhibición.'
-                });
-            } else {
-                res.json({sucess: true, message: "OK"});
-            }
-        });
     
-    // } catch (e){
-    //     res.status(500).json({
-    //         success: false,
-    //         message: 'Hubo un error al insertar el exhibición.'
-    //     });
-    // }
+    const sql = 'INSERT INTO `exhibiciones` set ?';
+
+    query(sql, req.body)
+        .then(() => {
+            res.json({
+                sucess: true,
+                message: "OK"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                success: false,
+                message: 'Hubo un error al insertar el exhibición.'
+            });
+        })
+    
 });
 
 module.exports = router
