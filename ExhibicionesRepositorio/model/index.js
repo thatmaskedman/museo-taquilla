c23dc1ae67bd13356155c861529c9b4d36ef0d13
oscarsil <osgisilo@gmail.com>
2022-01-02 01:53:00 -0700

    modelo de update price hecho

--- ExhibicionesRepositorio/model/index.js
+++ ExhibicionesRepositorio/model/index.js
@@ -1,6 +1,7 @@
 const { query } = require('../../db');
 
 const SELECT = `SELECT * FROM exhibiciones`;
+const UPDATE_PRICE = `UPDATE exhibiciones `;
 
 /**
  * Get a listing of the exhibitions.
@@ -19,5 +20,15 @@ const list  = async ({ available = false }) => {
                 .then(res => res)
                 .catch(err => {throw err});
 }
+/**
+ * update price
+ * @id {int} id de la exhibicion que se quiere cambiar
+ * @newprice {float} el nuevo precio para la exhibicion
+ * @returns {void}
+ */
+const update_exh_price = async (id, newprice) => {
+    await query(`${UPDATE_PRICE} SET precio = ${newprice} WHERE id = ${id}`)
+                .catch(err => { throw err });
+}
 
-module.exports = { list };
+module.exports = { list, update_exh_price };
