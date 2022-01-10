02f42719fdf9f040984761081737e9c9631c09f0
Jordan <bxordan@gmail.com>
2022-01-09 23:28:26 -0700

    adding model append to database method

--- PromocionesRepositorio/model/index.js
+++ PromocionesRepositorio/model/index.js
@@ -1,6 +1,7 @@
 const { query } = require('../../db');
 
 const SELECT = `SELECT * FROM promociones`;
+const INSERT = 'INSERT into promociones SET ?'
 
 const list = async() => {
     return await query(`${SELECT} WHERE vigencia >= CURDATE()`)
@@ -8,4 +9,9 @@ const list = async() => {
     .catch(err => {throw err});
 }
 
-module.exports = {list};
+const append = async (data) => {     
+    return await query(INSERT, data)
+        .catch(err => { throw err });
+}
+
+module.exports = {list, append};
