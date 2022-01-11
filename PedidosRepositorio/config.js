9d3ca58e4b9a52af8315aad3ddf5fc63ee8feff6
ger88555 <germanadolf@protonmail.com>
2022-01-10 19:18:48 -0700

    fix typo

--- PedidosRepositorio/config.js
+++ PedidosRepositorio/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     host: process.env.HOST || 'localhost',
     port: process.env.HEROKU ? 6000 : process.env.PORT || 6000, // override heroku port
-    storage: './storage/'
+    storage: './storage/',
+    motherlode: !!process.env.DISABLE_BANK
 }
--- PedidosRepositorio/utils/bank.js
+++ PedidosRepositorio/utils/bank.js
@@ -1,8 +1,12 @@
 const { Transacciones } = require('transacciones')
+const { motherlode } = require('../config')
 
 Transacciones({
     url: process.env.BANK_API_URL || undefined,
     cuenta: process.env.BANK_ACCOUNT || '4539218311924617'
 })
 
-module.exports = Transacciones()
+if (motherlode)
+    module.exports = { recibir: () => ({ id_Transaccion : null }) }
+else
+    module.exports = Transacciones()
